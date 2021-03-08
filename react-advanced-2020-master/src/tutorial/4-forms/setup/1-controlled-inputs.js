20061b9d2c566d07540a57e9d2f670858bf592be
Homayoun Ostad <hhhomayoun@yahoo.com>
2021-03-08 00:19:44 -0800

    learned forms

--- react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { getByDisplayValue } from '@testing-library/dom'
+import React, { useState } from 'react'
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +7,72 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
-};
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
 
-export default ControlledInputs;
+  const handleSub = (e) => {
+    e.preventDefault()
+    if (name && email) {
+      const person = { id: new Date().getTime().toString(), name, email }
+      console.log(person)
+      setPeople((people) => {
+        return [...people, person]
+      })
+      setName('')
+      setEmail('')
+    } else {
+      console.log('empty values')
+    }
+  }
+
+  return (
+    <>
+      <article>
+        <form className='form'>
+          <div className='form-control'>
+            <label htmlFor='firstName'>name:</label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value)
+              }}
+            />
+          </div>
+        </form>
+        <form className='form'>
+          <div className='form-control'>
+            <label htmlFor='email'>email:</label>
+            <input
+              type='text'
+              id='email'
+              name='email'
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+            />
+          </div>
+        </form>
+        <button type='button' onClick={handleSub}>
+          submit
+        </button>
+
+        {people.map((person) => {
+          const { name, email, id } = person
+          return (
+            <div className='item' key={id}>
+              <h4>{name}</h4>
+              <p>{email}</p>
+            </div>
+          )
+        })}
+      </article>
+    </>
+  )
+}
+
+export default ControlledInputs
